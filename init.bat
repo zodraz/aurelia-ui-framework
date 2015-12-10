a730584951a7fc2d8df1dba928d373727751f18d
Adarsh Pastakia <adarshpastakia@outlook.com>
2015-12-10 13:11:04 +0400

    Added batch for setting up on windows

---
+++ init.bat
@@ -0,0 +1,15 @@
+npm init
+
+npm install jspm gulp gulp-compass gulp-plumber gulp-typescript aurelia-bundler merge2 browser-sync run-sequence --save-dev
+
+jspm init
+
+jspm install text
+jspm install jquery
+jspm install core-js
+jspm install aurelia-router
+jspm install aurelia-logging
+jspm install aurelia-framework
+jspm install aurelia-validation
+jspm install aurelia-bootstrapper
+jspm install aurelia-ui-framework=github:adarshpastakia/aurelia-ui-framework
