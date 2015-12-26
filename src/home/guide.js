3374c2a965aba2e6a3ed31e2767132a06480b8f0
Adarsh Pastakia <adarshpastakia@outlook.com>
2015-12-26 20:17:03 +0400

    Added framework cheat sheet

---
+++ src/home/guide.js
@@ -0,0 +1,8 @@
+define(["require", "exports"], function (require, exports) {
+    var HomeGuide = (function () {
+        function HomeGuide() {
+        }
+        return HomeGuide;
+    })();
+    exports.HomeGuide = HomeGuide;
+});
