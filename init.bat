7cf953e6158aa97bd44fd7dadfad71a2ecf87dae
Adarsh Pastakia <adarshpastakia@outlook.com>
2016-04-17 17:06:24 +0400

    Added init.bat

---
+++ init.bat
@@ -0,0 +1,21 @@
+call npm init
+
+call npm install jspm gulp gulp-compass gulp-plumber gulp-typescript aurelia-bundler merge2 browser-sync run-sequence --save-dev
+
+call jspm init
+
+call jspm install text
+call jspm install fetch
+call jspm install lodash
+call jspm install moment
+call jspm install numeral
+call jspm install aurelia-ui-framework
+call jspm install aurelia-bootstrapper
+call jspm install aurelia-validation
+call jspm install aurelia-fetch-client
+call jspm install aurelia-framework
+call jspm install aurelia-metadata
+call jspm install aurelia-router
+call jspm install aurelia-logging
+call jspm install aurelia-event-aggregator
+call jspm install aurelia-templating-resources
